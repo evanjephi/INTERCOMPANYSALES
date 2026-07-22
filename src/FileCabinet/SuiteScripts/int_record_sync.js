c91999887252ce42c42670f09b23122b7a82ad41
Even Yohans <evanjephi@gmail.com>
2026-07-22 09:07:05 -0400

    updated IDs

--- src/FileCabinet/SuiteScripts/int_record_sync.js
+++ src/FileCabinet/SuiteScripts/int_record_sync.js
@@ -6,11 +6,12 @@
 define(['N/record', 'N/runtime', 'N/log'], function (record, runtime, log) {
     function onAction(context) {
         const rt = runtime.getCurrentScript()
-       
+
         const nr = context.newRecord
         const typeselector = {
             1: 1, 2: 2, 7: 3, 9: 14, 15: 12, 14: 9, 13: 8
         }
+        let requester = nr.getText('custbody_project_manager') ? nr.getText('custbody_project_manager') : nr.getText('salesrep')
         const crpo = record.create({
             type: 'purchaseorder',
             isDynamic: true
@@ -18,7 +19,7 @@ define(['N/record', 'N/runtime', 'N/log'], function (record, runtime, log) {
 
         crpo.setValue({
             fieldId: 'entity',
-            value: 10954
+            value: 10594
         })
         crpo.setValue({
             fieldId: 'custbody_ship_ref_so',
@@ -26,7 +27,7 @@ define(['N/record', 'N/runtime', 'N/log'], function (record, runtime, log) {
         })
         crpo.setValue({
             fieldId: 'memo',
-            value: `Project Name: ${nr.getValue('custbody_viso_project')} 1. Please put your delivery date on the invoice. 2. Please put our PO number on the invoice. 3. Each Carton must be labeled with: PO number, Part number, Project name, Quantity and Carton number. 4. Please refer to drawings for more details. 5. Please sign the work back schedule.`
+            value: `Project Name: ${nr.getText('custbody_viso_project')} 1. Please put your delivery date on the invoice. 2. Please put our PO number on the invoice. 3. Each Carton must be labeled with: PO number, Part number, Project name, Quantity and Carton number. 4. Please refer to drawings for more details. 5. Please sign the work back schedule.`
         })
         crpo.setValue({
             fieldId: 'duedate',
@@ -37,16 +38,16 @@ define(['N/record', 'N/runtime', 'N/log'], function (record, runtime, log) {
             value: nr.getValue('custbody_viso_project')
         })
         crpo.setValue({
+            fieldId: 'custbodyrequestedby',
+            value: requester
+        })
+        crpo.setValue({
             fieldId: 'location',
             value: nr.getValue('location')
         })
         crpo.setValue({
             fieldId: 'terms',
             value: nr.getValue('terms')
-        })
-        crpo.setValue({
-            fieldId: 'class',
-            value: typeselector[nr.getValue('ordertype')]
         })
 
         const len = nr.getLineCount({ sublistId: 'item' })
@@ -74,6 +75,10 @@ define(['N/record', 'N/runtime', 'N/log'], function (record, runtime, log) {
             })
             crpo.commitLine({ sublistId: 'item' })
         }
+        crpo.setValue({
+            fieldId: 'class',
+            value: typeselector[nr.getValue('ordertype')]
+        })
         const crpoId = crpo.save()
         log.debug('Execution reached end of scripit', 'POID: ' + crpoId + ', Remaining Usage: ' + rt.getRemainingUsage())
     }
