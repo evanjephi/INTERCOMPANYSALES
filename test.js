3ab9c7bb4482e91c8b1ca08de8dcfc769333199f
Even Yohans <evanjephi@gmail.com>
2026-07-28 11:54:18 -0400

    a test to typeselctor

---
+++ test.js
@@ -0,0 +1,8 @@
+        const typeselector = {
+            1: 1,2: 2, 7: 3, 9: 14,15: 12, 14: 9,13: 8
+        }
+
+        const arr = ['e3', 'e4', 'e5', 'e6']
+        arr.push('e7')
+        arr.splice(1,3)
+        console.log('arr', arr)
